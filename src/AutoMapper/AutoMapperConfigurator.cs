b30a3b3ac0a449cd16b8a6a6b3821bb8d8d52d18
Shadi Mahmoodian <shadimahm@gmail.com>
2018-10-28 14:07:31 +0330

    Added Auto Mapper to Hydrogen

---
+++ src/AutoMapper/AutoMapperConfigurator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using hydrogen.General.Collections;
+
+namespace hydrogen.AutoMapper
+{
+    public static class AutoMapperConfigurator
+    {
+        public static void Scan(Assembly assembly)
+        {
+             assembly.GetTypes()
+                .Where(t => t.GetCustomAttributes(typeof(AutoMapperConfigAttribute)).Any())
+                .OrderBy(GetDistanceFromObject)
+                .ForEach(type =>
+                {
+                    var method = type.GetMethod("ConfigureAutoMapper", new Type[0]);
+                    if (method == null || !method.IsStatic || method.ReturnType != typeof(void) || method.GetParameters().Any())
+                        throw new InvalidOperationException(
+                            "Type " + type.FullName + " is decorated with [AutoMapperConfigAttribute] but does not contain a public static method with the signature of 'void ConfigureAutoMapper()'");
+
+                    method.Invoke(null, null); 
+                });
+        }
+
+        private static int GetDistanceFromObject(Type t)
+        {
+            var result = 0;
+            while (!(typeof(object) == t))
+            {
+                if (t == null)
+                    return -1;
+
+                result++;
+                t = t.BaseType;
+            }
+
+            return result;
+        }
+    }
+}
